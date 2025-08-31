f82090c22b7cd87543990bcf158ae117b364242c
jacky.kao <jackykao528@outlook.com>
2025-08-31 17:42:11 +0800

    mail service

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Mailer Mail
+}
+
+const webPort = "80"
+
+func main() {
+	app := Config{
+		Mailer: createMail(),
+	}
+
+	log.Printf("Starting Mail Service API on port %s\n", webPort)
+
+	// define the server
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+
+	return m
+}
